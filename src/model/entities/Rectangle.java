3082dd691fabcb1d9d853923ff71976826549892
Walter dos Santos <walter4asantos@gmail.com>
2020-09-05 16:11:40 -0300

    Solução com classe abstrata somente

---
+++ src/model/entities/Rectangle.java
@@ -0,0 +1,36 @@
+package model.entities;
+
+import model.enums.Color;
+
+public class Rectangle extends Shape {
+
+	private Double width;
+	private Double hight;
+	
+	public Rectangle(Color color, Double width, Double hight) {
+		super(color);
+		this.width = width;
+		this.hight = hight;
+	}
+
+	public Double getWidth() {
+		return width;
+	}
+
+	public void setWidth(Double width) {
+		this.width = width;
+	}
+
+	public Double getHight() {
+		return hight;
+	}
+
+	public void setHight(Double hight) {
+		this.hight = hight;
+	}
+
+	@Override
+	public double area() {
+		return width * hight;
+	}
+}
